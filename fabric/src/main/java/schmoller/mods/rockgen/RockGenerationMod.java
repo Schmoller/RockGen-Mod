51f215a38a3aebd2b5af2ec916fbb4f6959bdab6
Steven Schmoll <steven.schmoll@gmail.com>
2023-01-01 16:12:29 +1100

    Fabric: Do some light configuration to get us ready for development

---
+++ fabric/src/main/java/schmoller/mods/rockgen/RockGenerationMod.java
@@ -0,0 +1,10 @@
+package schmoller.mods.rockgen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RockGenerationMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
